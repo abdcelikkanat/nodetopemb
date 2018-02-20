421ad92f40bd655deeb5855f595d388642fca1e1
Abdulkadir Çelikkanat <abdcelikkanat@gmail.com>
2018-02-20 18:17:46 +0100

    .

---
+++ twe1/get_topics.py
@@ -0,0 +1,54 @@
+#/home/abdulkadir/anaconda2/envs/embedding-tests/bin python
+#import gensim
+import oldgensim.gensim as gensim2
+
+def get_topics(walks_file, output_topic_file, number_of_topics, number_of_nodes):
+
+    # Read the document -> number of walks
+    ## It is assumed that the file consists of one line
+    document = []
+    nodes = []
+
+    with open(walks_file) as f:
+        for line in f:
+            document.append([v for v in line.strip().split() if v])
+
+
+    nodes = [i for i in range(number_of_nodes)]
+
+
+    # Set the dictionary
+    dict = gensim.corpora.Dictionary([[unicode(node) for node in range(number_of_nodes)]])
+
+    # Extract the corpus
+    corpus = [dict.doc2bow(doc) for doc in document]
+    #corpus = [dict.doc2bow(document)]
+
+
+    # Run LDA
+    #id2word = {i:v for i, v in dict.items()}
+    id2word = dict
+    lda = gensim2.models.ldamodel.LdaModel(corpus=corpus, id2word=id2word, num_topics=number_of_topics)
+
+    # Find the topic assignments of each word
+    clusters = [[] for _ in range(number_of_topics)]
+
+    word2topic = {}
+    for word in dict.values():
+        top_prob = lda.get_term_topics(dict.token2id[word], minimum_probability=-1.0)
+        word2topic.update({word: max(top_prob, key=lambda item:item[1])[0]})
+        clusters[word2topic[word]].append(word)
+
+    topics = []
+    for doc in document:
+        for word in doc:
+            topics.append(unicode(word2topic[word]))
+
+    with open(output_topic_file, 'w') as f:
+        f.write(" ".join(topics))
+
+    for i in range(number_of_topics):
+        print("The cluster {} contains {} number of nodes.".format(i, len(clusters[i])))
+
+
+    return word2topic
