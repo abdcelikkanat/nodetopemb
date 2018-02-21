902df87ae783e9799708854db858db96cccd6714
Abdulkadir Çelikkanat <abdcelikkanat@gmail.com>
2018-02-21 18:10:47 +0100

    node2vec

--- twe1/generate_walks_corpus.py
+++ twe1/generate_walks_corpus.py
@@ -3,7 +3,7 @@ from nodetopemb import Graph
 import random
 
 
-def saveGraphWalks(graph, output_walks_file, num_of_paths, path_length, num_of_documents, together=True):
+def saveGraphWalks(graph, output_walks_file, num_of_paths, path_length, num_of_documents, params, method, together=True):
 
     data_size = num_of_paths * path_length * graph.number_of_nodes()
 
@@ -14,7 +14,9 @@ def saveGraphWalks(graph, output_walks_file, num_of_paths, path_length, num_of_d
         for _ in range(num_of_documents):
             document = graph.graph2doc(number_of_paths=num_of_paths,
                                        path_length=path_length,
-                                       alpha=0.0, rand=random.Random())
+                                       params=params, rand=random.Random(),
+                                       method=method)
+
             if together:
                 # Save documents line by line
                 f.write(u"{}\n".format(u" ".join(str(node) for line in document for node in line)))
