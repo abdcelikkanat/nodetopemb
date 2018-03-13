a379608e9d72d4cbc35f5057a0de0d5e0bf88490
Abdulkadir Çelikkanat <abdcelikkanat@gmail.com>
2018-03-13 17:53:28 +0100

    overlapping basinda

--- test/community.py
+++ test/community.py
@@ -3,7 +3,7 @@ import networkx as nx
 import matplotlib.pyplot as plt
 
 def get_nb_list(g):
-    return [[int(nb) for nb in nx.neighbors(g, node)] for node in range(g.number_of_nodes())]
+    return [[int(nb) for nb in nx.neighbors(g, str(node))] for node in range(g.number_of_nodes())]
 
 example1 = [[0, 1], [0, 2], [0, 3], [1, 2], [1, 3], [2, 3], [4, 5], [4,6], [4,7], [5,6], [5,7], [6,7], [3,4]]
 
@@ -11,8 +11,10 @@ example1 = [[0, 1], [0, 2], [0, 3], [1, 2], [1, 3], [2, 3], [4, 5], [4,6], [4,7]
 edges = example1
 
 
-g = nx.Graph()
-g.add_edges_from(edges)
+#g = nx.Graph()
+#g.add_edges_from(edges)
+
+g = nx.read_gml("../datasets/karate.gml")
 
 """
 plt.figure()
@@ -21,19 +23,24 @@ plt.show()
 """
 
 N = g.number_of_nodes()
-D = 2
+D = 1
 K = 2
-eta = 0.001
+eta = 0.01
 
-F = np.random.normal(size=(N, K, D))*10.0
+F = np.random.normal(size=(N, K, D))*1.0
 nb_list = get_nb_list(g)
 
 F = np.abs(F)
 
-num_of_iters = 2000
+logF = 0.0
+
+num_of_iters = 10000
 for iter in range(num_of_iters):
 
+    if iter % 100 == 0:
+        print("Iter {}, logF {}".format(iter, logF))
     logF = 0.0
+
     for node in range(N):
         ft = 0.0
         for nb in nb_list[node]:
@@ -47,7 +54,7 @@ for iter in range(num_of_iters):
 
         logF += (ft - lt)
 
-    print(logF)
+    #print(logF)
 
     for node in range(N):
 
@@ -91,7 +98,7 @@ for iter in range(num_of_iters):
 M = np.arange(0, 27).reshape((3,3,3))
 
 
-print(F)
+print(logF)
 
 edges = [[np.dot(F[node, :, 0], F[nb, :, 0]) for nb in range(N)] for node in range(N) ]
 print(np.asarray(edges))
@@ -100,6 +107,8 @@ for node in range(N):
     for k in range(K):
         print("Node: {}, Cluster: {} Val: {}".format(node, k, np.sum(F[node, k, :]**1)))
 
+
+"""
 plt.figure()
 plt.plot(F[:4, 0, 0], F[:4, 0, 1], 'r.')
 plt.plot(F[:4, 1, 0], F[:4, 1, 1], 'b.')
@@ -107,4 +116,16 @@ plt.plot(F[4:, 0, 0], F[4:, 0, 1], 'rx')
 plt.plot(F[4:, 1, 0], F[4:, 1, 1], 'bx')
 plt.show()
 
-""" """
+"""
+x = np.asarray([[F[node, k, 0] for k in range(K)] for node in range(N)])
+
+"""
+plt.figure()
+plt.plot(x[:4,0], x[:4, 1], 'x')
+plt.plot(x[4:,0], x[4:, 1], 'rx')
+plt.show()
+"""
+
+plt.figure()
+plt.plot(x[:, 0], x[:, 1], 'x')
+plt.show()
